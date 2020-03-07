d4405fbb2dd7d8713208c18eb00627f67f8f7750
sagie gur ari <sagiegurari@gmail.com>
2020-03-07 21:56:05 +0000

    windows fixes

--- duckscript_sdk/src/sdk/std/env/os_version/mod_test.rs
+++ duckscript_sdk/src/sdk/std/env/os_version/mod_test.rs
@@ -7,7 +7,14 @@ fn common_functions() {
     test::test_common_command_functions(create(""));
 }
 
+#[cfg(windows)]
 #[test]
+fn run_windows() {
+    test::run_script_and_error(vec![create("")], "out = os_version", "out");
+}
+
+#[test]
+#[cfg(not(windows))]
 fn run_valid() {
     test::run_script_and_validate(
         vec![create("")],
