a555be4805341ef1946c1e101f04b9964c145248
sagie gur ari <sagiegurari@gmail.com>
2020-03-07 22:04:16 +0000

    windows fixes

--- duckscript_sdk/src/sdk/std/env/os_release/mod_test.rs
+++ duckscript_sdk/src/sdk/std/env/os_release/mod_test.rs
@@ -1,24 +1,27 @@
 use super::*;
 use crate::test;
-use crate::test::CommandValidation;
 
 #[test]
 fn common_functions() {
     test::test_common_command_functions(create(""));
 }
 
-#[cfg(windows)]
-#[test]
-fn run_windows() {
-    test::run_script_and_error(vec![create("")], "out = os_release", "out");
-}
+cfg_if::cfg_if! {
+    if #[cfg(windows)] {
+        #[test]
+        fn run_windows() {
+            test::run_script_and_error(vec![create("")], "out = os_release", "out");
+        }
+    } else {
+        use crate::test::CommandValidation;
 
-#[test]
-#[cfg(not(windows))]
-fn run_valid() {
-    test::run_script_and_validate(
-        vec![create("")],
-        "out = os_release",
-        CommandValidation::Ignore,
-    );
+        #[test]
+        fn run_valid() {
+            test::run_script_and_validate(
+                vec![create("")],
+                "out = os_release",
+                CommandValidation::Ignore,
+            );
+        }
+    }
 }
--- duckscript_sdk/src/sdk/std/env/os_version/mod_test.rs
+++ duckscript_sdk/src/sdk/std/env/os_version/mod_test.rs
@@ -1,24 +1,27 @@
 use super::*;
 use crate::test;
-use crate::test::CommandValidation;
 
 #[test]
 fn common_functions() {
     test::test_common_command_functions(create(""));
 }
 
-#[cfg(windows)]
-#[test]
-fn run_windows() {
-    test::run_script_and_error(vec![create("")], "out = os_version", "out");
-}
+cfg_if::cfg_if! {
+    if #[cfg(windows)] {
+        #[test]
+        fn run_windows() {
+            test::run_script_and_error(vec![create("")], "out = os_version", "out");
+        }
+    } else {
+        use crate::test::CommandValidation;
 
-#[test]
-#[cfg(not(windows))]
-fn run_valid() {
-    test::run_script_and_validate(
-        vec![create("")],
-        "out = os_version",
-        CommandValidation::Ignore,
-    );
+        #[test]
+        fn run_valid() {
+            test::run_script_and_validate(
+                vec![create("")],
+                "out = os_version",
+                CommandValidation::Ignore,
+            );
+        }
+    }
 }
